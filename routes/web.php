88bd208e76354f506aa0c80513c31b19245daa25
Sebastian <privat@sebhallin.se>
2021-10-05 21:43:49 +0200

    Add authenticate route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/authenticate', [LoginController::class, 'authenticate'])
+    ->name('auth.login.authenticate');
